b35d4ff6cfa1a2f0288cc8ff14a4ddad7ab6f010
yumarie <yumarie@umich.edu>
2022-05-10 14:48:01 -0400

    Format code and improved leaderboard display

--- botcommands.py
+++ botcommands.py
@@ -1,5 +1,14 @@
 import requests
 import json
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get the API token from the .env file.
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
 #Every minute, update which players are online or offline
 def update_players():
@@ -12,18 +21,18 @@ def update_chests_status():
 #Returns a dictionary of the top 100 in a specific leaderboard
 def get_leaderboard(type, timeframe):
     #If the user entered player for the type of leaderboard, GET the player leaderboard api data
-    if (type == "Player" or type == "player"):
+    if (type.lower() == "player"):
         response_API = requests.get('https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=player&timeframe=alltime')
     #If the user entered pvp for the type of leaderboard, GET pvp data
-    elif (type == "PVP" or type == "pvp"):
+    elif (type.lower() == "pvp"):
         #If the user entered alltime, get alltime pvp data
-        if timeframe == "alltime":
+        if timeframe.lower() == "alltime":
             response_API = requests.get('https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=pvp&timeframe=alltime')
         #GET weekly pvp data
-        elif timeframe == "weekly":
+        elif timeframe.lower() == "weekly":
             response_API = requests.get('https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=pvp&timeframe=weekly')
     #If the user entered guild for the type of leaderboard, GET the guild data
-    elif (type == "Guild" or type == "guild"):
+    elif (type.lower() == "guild"):
         response_API = requests.get("https://api.wynncraft.com/public_api.php?action=statsLeaderboard&type=guild&timeframe=alltime")
     #If player, pvp, or guild was not entered, the user mispelled or used the command improperly. 
     #Return an empty dictionary for error handling.
@@ -36,10 +45,8 @@ def get_leaderboard(type, timeframe):
     #return the dictionary
     return parsed_data
 
-
-from discord.ext import commands
-#Every command you type into discord must be prefixed with the !
-bot = commands.Bot(command_prefix='!')
+#Every command you type into discord must be prefixed with the .
+bot = commands.Bot(command_prefix='.')
 
 #When the python code has connected to the bot and activiated the bot online, print this statement
 @bot.event
@@ -77,28 +84,28 @@ async def leaderboard(message, type, scope, timeframe):
             await message.send("Incorrect usage! \nTo use, format as: !leaderboard string<type> int<scope> string<timeframe>")
             return #empty return statement, exits this entire function
         #sets the scope maximum limit to 30
-        if int(scope) > 30: scope = "30"
-        #initializes the string which will store the leaderboard
-        board = ""
+        if int(scope) > 100: scope = "100"
+        #initializes the embedded discord text which will store the leaderboard
+        board = discord.Embed(
+                title = type.lower().capitalize() + "Leaderboard",
+                color = discord.Color.green()
+        )
         #loops through the top <scope> players, storing their information in the string
         for i in range (0, int(scope)):
             #creates the rank (arrays start at index 0, so index 0 is no. 1)
             rank = str(i + 1)
-            #stores basic information, rank and name
-            board += rank + ". " + data["data"][i]["name"] + "   "
             #stores different stats depending on which type of leaderboard it is
-            if type == "Player" or type == "player":
-                board += "combat level/xp: " + str(data["data"][i]["xp"]) + "\n"
+            if type.lower() == "player":
+                board.add_field(name= rank + ". " + data["data"][i]["name"], value = "combat level/xp: " + str(data["data"][i]["xp"]))
             if type == "PVP" or type == "pvp":
-                board += "kills: " + str(data["data"][i]["kills"]) + "\n"
+                board.add_field(name = rank + ". " + data["data"][i]["name"], value = "kills: " + str(data["data"][i]["kills"]))
             if type == "Guild" or type == "guild":
-                board += "territories:" + str(data["data"][i]["territories"]) + "\n"
+                board.add_field(name = rank + ". " + data["data"][i]["name"], value = "territories:" + str(data["data"][i]["territories"]))
         #prints the leaderboard onto the discord channel 
-        await message.send(board)
+        await message.send(embed=board)
     #If the dictionary returned by the function is empty, we know the command has been misused and simply exit with an error message
     else:
         await message.send("Incorrect usage! \nTo use, format as: !leaderboard string<type> int<scope> string<timeframe>")
 
 #Connects the python script to the discord bot and keeps the bot running until the code terminates. 
-#Uses an authorization token. Keep this token secret! 
-bot.run('OTczMzkxMTczMTQ0MTc4NzU4.GGHNIO.TvqNcHEgZ7lNvw4qhYuVYfq3D91I4lLq4bcJdw')
+bot.run(DISCORD_TOKEN)
